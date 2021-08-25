e01ec12190fdc98391d9afba889125d827c81472
LAPTOP-7ORUAPFA\myoung.e <mythe1004@naver.com>
2021-08-25 19:44:59 +0900

    21-08-25 quiz03 jsp뷰 화면 띄우기

---
+++ src/main/java/com/quiz/lesson01/Quiz03Controller.java
@@ -0,0 +1,14 @@
+package com.quiz.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Quiz03Controller {
+	
+	// 요청URL : http://localhost/lesson01/quiz03
+	@RequestMapping("/lesson01/quiz03")
+	public String quiz03() {
+		return "lesson01/quiz03";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/lesson01/quiz03.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Quiz03</title>
+</head>
+<body>
+	<h2>리스트 기억 나시죠?</h2>
+	
+	<h3>나의 최애 메뉴!</h3>
+	<ul>
+		<li>마라샹궈</li>
+		<li>알리오올리오</li>
+		<li>과카몰리</li>
+	</ul>
+</body>
+</html>
